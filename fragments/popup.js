c1c2a8a638efbdcfb5138e364da0eba80849f506
Owais <owais.khan@additiv.ch>
2017-07-01 13:26:27 +0500

    add comments

--- fragments/popup.js
+++ fragments/popup.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$("#authenticate").click(function(){
-    var CLIENT_ID = '741_20atolbhuhq8wwsgk8kwowc888wocgwkk8o4gsw8os408owkc4';
-    var REDIRECT_URI = 'https://example.com/auth';
+    var CLIENT_ID = '741_20atolbhuhq8wwsgk8kwowc888wocgwkk8o4gsw8os408owkc4'; //CLIENT_ID: configured in TimeDoctor App
+    var REDIRECT_URI = 'https://example.com/auth'; //REDIRECT_URI: configured in TimeDoctor App
     var url = 'https://webapi.timedoctor.com/oauth/v2/auth?client_id=' + CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=token';
-    chrome.tabs.create({url: url});
+    chrome.tabs.create({url: url}); //create new tab and authenticate the TimeDoctor App using the url format in TimeDoctor WebAPI
 	});
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-  var access_token = urlParam('access_token');
+  var access_token = urlParam('access_token'); //get the access_token which is used in other requests using the WebAPI
   console.log(access_token);
   if(access_token != null && access_token != ""){
-    $("body").empty();
-    $("body").append("<div id='resDiv'></div>");
-    var get_companies_url = "https://webapi.timedoctor.com/v1.1/companies?access_token=" + access_token;
+    $("body").empty(); //clear the whole body of the webpage
+    $("body").append("<div id='resDiv'></div>"); // append a clear div with id 'resDiv' in the body
+    var get_companies_url = "https://webapi.timedoctor.com/v1.1/companies?access_token=" + access_token; // WebAPI request url for getting the companies
 
-    $.get(get_companies_url, function(data, status){
+    $.get(get_companies_url, function(data, status){ //get companies using the WebAPI of TimeDoctor
       console.log(data);
       $("#resDiv").append("<h1>Users</h1>");
       var companies = data.accounts;
-      $.each(companies, function(index,company){
-        var get_users_url = "https://webapi.timedoctor.com/v1.1/companies/" + company.company_id + "/users?access_token=" + access_token;
-        $.get(get_users_url, function(userdata, userstatus){
+      $.each(companies, function(index,company){ //loop through the companies and get the company_id which is required for the USERS request and show All users in the body(div with resDiv) of the webpage
+        var get_users_url = "https://webapi.timedoctor.com/v1.1/companies/" + company.company_id + "/users?access_token=" + access_token; // WebAPI request url for getting the users in company; Requirements: company_id
+        $.get(get_users_url, function(userdata, userstatus){ //get users of a company using the WebAPI of TimeDoctor
           var users = userdata.users;
-          $.each(users, function(index,user){
+          $.each(users, function(index,user){ //loop through the users and append in the body of the webpage
             $("#resDiv").append("<span>" + user.full_name + "</span>&nbsp;&nbsp;&nbsp;<span>" + user.email + "</span>&nbsp;&nbsp;&nbsp;<span>" + user.level + "</span>&nbsp;&nbsp;&nbsp;<span><a href='javascript:;' class='showTasks' data-user-id='" + user.user_id + "' data-company-id='" + user.company_id + "'>Show Tasks</a></span>");
             $("#resDiv").append("<br><br>");
           });
@@ -24,14 +24,19 @@ $(document).ready(function(){
     });
   }
 
+  /*
+  * Click on the 'Show Tasks' link,
+  * showing respective user Tasks
+  * Requirements: user_id, company_id
+  */
   $('body').on('click', 'a.showTasks', function() {
     var user_id = $(this).data("user-id");
     var company_id = $(this).data("company-id");
     console.log(user_id, company_id);
-    var get_users_tasks_url = "https://webapi.timedoctor.com/v1.1/companies/" + company_id + "/users/" + user_id + "/tasks?access_token=" + access_token;
+    var get_users_tasks_url = "https://webapi.timedoctor.com/v1.1/companies/" + company_id + "/users/" + user_id + "/tasks?access_token=" + access_token; // WebAPI request url for getting the tasks of a user in a company; Requirements: company_id, user_id
     $.get(get_users_tasks_url, function(data, status){
       var tasks = data.tasks;
-      if(tasks.length > 0){
+      if(tasks.length > 0){ //if there were tasks for the clicked user; show the tasks
         console.log(tasks);
         $("#resDiv").append("<h1>Tasks</h1>");
         $.each(tasks, function(index, task){
@@ -43,13 +48,18 @@ $(document).ready(function(){
     });
   });
 
-  function urlParam(name){
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  /*
+  * @params: param_key e.g. 'access_token' in the url htpp://example.com?res=abc&access_token=ERFF67HB767GTIOVBW7
+  * Provide the 'url param key'
+  * returns value of the param_key from the url if matched
+  */
+  function urlParam(param_key){
+    var results = new RegExp('[\?&]' + param_key + '=([^&#]*)').exec(window.location.href);
     if (results==null){
       return null;
     }
     else{
-      return decodeURI(results[1]) || 0;
+      return decodeURI(results[1]) || 0; //decode the URI
     }
   }
 });
